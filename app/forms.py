c2d545ae312a79f3248d34426db9a1ab23675667
Ziyu Zhou <zz2768@columbia.edu>
2020-10-22 16:31:03 +0000

    Add field

--- app/forms.py
+++ app/forms.py
@@ -4,4 +4,4 @@ from django.forms import ModelForm
 class Form(ModelForm):
     class Meta:
         model=squirrel
-        fields=['latitude','longitude','squirrel_id','shift','date','age']
+        fields=['latitude','longitude','squirrel_id','shift','date','age','primary_fur_color','location','specific_location','running','chasing','climbing','eating','foraging','other_Activities','kuks','quaas','moans','tail_flags','tail_twitches','approaches','indifferent','runs_From']
